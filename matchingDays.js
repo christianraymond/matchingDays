cd5da5ecf4322cc7172d7aeb6735ce047b9d0c73
christianraymond <ombenisn.raymond@gmail.com>
2017-04-03 17:12:27 +0200

    second commit

--- matchingDays.js
+++ matchingDays.js
@@ -1,3 +1,28 @@
-<button type="button" 
-onclick="document.getElementById('day').style.color = 'grey'">
-Click Me!</button>
+var days = document.querySelectorAll(".day");
+var firstDate = document.querySelector("#input1");
+var secondDate = document.querySelector("#input2");
+var userButton = document.querySelector("#myButton");
+
+
+userButton.addEventListener("click", function(){
+var date1 = new Date(firstDate.value);
+var date2 = new Date(secondDate.value);
+
+var dayOneValue = date1.getDay("date1")
+var dayTwoValue = date2.getDay("date2")
+
+for (var i = 0; i < days.length; i++) {
+days[i]
+  days[i].classList.remove("style1");
+  days[i].classList.remove("style2");
+  days[i].classList.remove("style3");
+}
+if(dayOneValue === dayTwoValue){
+  days[dayOneValue].classList.add("style1");
+}
+else{
+  days[dayOneValue].classList.add("style2");
+  days[dayTwoValue].classList.add("style3");
+}
+
+})
